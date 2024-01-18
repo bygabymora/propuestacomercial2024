274061659b388783ca01f46771b8a8ece870bf11
bygabymora <bygabymora@gmail.com>
2024-01-18 16:42:47 -0500

    Delete unused files and update dependencies

---
+++ pages/_app.js
@@ -0,0 +1,8 @@
+import React, { useEffect } from 'react';
+import '../public/globals.css';
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
