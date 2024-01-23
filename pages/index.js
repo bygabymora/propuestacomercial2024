94c040d5dcbd04dcc4671a905be446e76a977fbb
bygabymora <bygabymora@gmail.com>
2024-01-23 16:15:19 -0500

    Update title in RootLayout component

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ import Boton from '../public/images/boton.png';
 
 export default function Home() {
   return (
-    <RootLayout title="Formulario Medellín 2023">
+    <RootLayout title="Propuesta Comercial 2024">
       <main className=" bg-gray-50 pt-2">
         <div className="justify-center items-center w-full pt-2 pb-2 leading-tight ">
           <VideoContainer />
@@ -16,7 +16,7 @@ export default function Home() {
               <Image
                 src={Boton}
                 alt="Cuéntenos sus expectativas."
-                className="md:w-[90%] xl:w-full"
+                className=""
               />
             </Link>
           </div>
