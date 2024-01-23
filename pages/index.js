34ba8513211ed9e6b50a76fb57ecc0aa1a089387
bygabymora <bygabymora@gmail.com>
2024-01-23 18:27:51 -0500

    Refactor button container in index.js

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,8 @@ export default function Home() {
         <div className="justify-center items-center w-full pt-2 pb-2 leading-tight">
           <VideoContainer />
 
-          <div className=" fixed bottom-0 m-1 mt-2 p-1 items-center justify-center flex hide-on-mobile-landscape">
+          {/* Button container */}
+          <div className="fixed bottom-0 left-0 w-full p-2 flex justify-center items-center sm:relative sm:bottom-auto sm:w-auto sm:bg-transparent hide-on-mobile-landscape">
             <Link href="/formulario">
               <Image src={Boton} alt="Cuéntenos sus expectativas." />
             </Link>
