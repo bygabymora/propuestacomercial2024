ccf856ca38e4aa9504262aaa019d15a9725156b1
bygabymora <bygabymora@gmail.com>
2024-01-23 18:23:21 -0500

    Fix button position on mobile landscape

--- pages/index.js
+++ pages/index.js
@@ -12,19 +12,9 @@ export default function Home() {
         <div className="justify-center items-center w-full pt-2 pb-2 leading-tight">
           <VideoContainer />
 
-          <div className="m-1 mt-2 p-1 items-center justify-center flex hide-on-mobile-landscape">
+          <div className=" fixed bottom-0 m-1 mt-2 p-1 items-center justify-center flex hide-on-mobile-landscape">
             <Link href="/formulario">
               <Image src={Boton} alt="Cuéntenos sus expectativas." />
-            </Link>
-          </div>
-
-          <div className="fixed bottom-0 left-0 w-full p-2 bg-white flex justify-center items-center sm:hidden">
-            <Link href="/formulario">
-              <Image
-                src={Boton}
-                alt="Cuéntenos sus expectativas."
-                className="w-auto h-16"
-              />
             </Link>
           </div>
         </div>
